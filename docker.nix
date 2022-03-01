88e23c03add3cc1bb49c7481722b00eea14e6c77
Андрей Власов <aavlasov_3@miem.hse.ru>
2022-03-02 00:02:17 +0300

    Add docker.nix

---
+++ docker.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import <nixpkgs> {};
+  inherit (pkgs) dockerTools;
+
+  project = import ./.;
+  inherit (project) ruz-finder;
+  inherit (ruz-finder.identifier) name version;
+  ruz-finder-bin = ruz-finder.components.exes.ruz-finder;
+in dockerTools.buildImage {
+  inherit name;
+  contents = [ ruz-finder-bin ];
+  config = {
+    Entrypoint = [ "${ruz-finder-bin}/bin/ruz-finder" ];
+  };
+}
