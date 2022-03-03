b27f5c13dd09c6ca30620bf209ba97bb30be1c74
Андрей Власов <aavlasov_3@miem.hse.ru>
2022-03-03 21:52:27 +0300

    Add built artefacts

--- docker.nix
+++ docker.nix
@@ -7,7 +7,9 @@ let
   inherit (ruz-finder.identifier) name version;
   ruz-finder-bin = ruz-finder.components.exes.ruz-finder;
 in dockerTools.buildImage {
-  inherit name version;
+  inherit name;
+  tag = version;
+
   contents = [ ruz-finder-bin ];
   config = {
     Entrypoint = [ "${ruz-finder-bin}/bin/ruz-finder" ];
